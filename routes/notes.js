93ca0512d3472a9954a58dd31fd64975c622a759
clayg <claytondguerrero@gmail.com>
2023-10-24 16:20:25 -0500

    added fsUtils.js and uuid.js from homeworks

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,15 @@
 const notesRoute = require("express").Router();
 const path = require("path");
+const uuid = require('../helpers/uuid')
 
 
 notesRoute.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/notes"));
+    //   res.sendFile(path.join(__dirname, "../public/notes"));
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 });
+
+notesRoute.post('/api/notes', (req, res) => {
+    readFromFile('./db/db.json')
+})
 
 module.exports = notesRoute;
